313a57618486b5b676d7d41de928339a3c00b668
Sanjay Purswani <sanjsanj@hotmail.com>
2016-09-25 19:45:11 +0100

    Can have adjustable mouth shapes

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class FaceView: UIView {
     var scale: CGFloat = 0.90
-    
-    var skullRadius: CGFloat {
+    var mouthCurvature: Double = 1.0 // 1 for full smile, -1 for full sade
+
+    private var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
     
-    var skullCenter: CGPoint {
+    private var skullCenter: CGPoint {
         return CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
@@ -64,10 +65,32 @@ class FaceView: UIView {
         return pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
     }
     
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratios.SkullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.SkullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.SkullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX + mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+        
+        return path
+    }
+    
     override func drawRect(rect: CGRect) {
         UIColor.blueColor().set()
         pathForCircleCenteredAtPoint(skullCenter, withRadius: skullRadius).stroke()
         pathForEye(.Left).stroke()
         pathForEye(.Right).stroke()
+        pathForMouth().stroke()
     }
 }
